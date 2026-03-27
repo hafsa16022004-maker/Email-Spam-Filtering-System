d62a26fdba442ad3ee72ef9aabca3de01ce872f7
hafsa16022004-maker <hafsa16022004@gmail.com>
2026-03-27 22:29:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,57 @@
+# import streamlit as st
+# import pickle
+# import string
+# from nltk.corpus import stopwords
+# import nltk
+# from nltk.stem.porter import PorterStemmer
+
+# ps = PorterStemmer()
+
+# def transform_text(text):
+#     text = text.lower()
+#     # breaking into separate words
+#     text = nltk.word_tokenize(text)
+
+#     # as text is converted to list after tokenization- so using loop
+#     y = []
+#     for i in text:
+#         if i.isalnum():  # just include alphanumeric- remove special characters
+#             y.append(i)
+
+#     text = y[:]  # removing stopwords and punctuation
+#     y.clear()
+
+#     for i in text:
+#         if i not in stopwords.words("english") and i not in string.punctuation:
+#             y.append(i)
+
+#     text = y[:]  # stemming  dancing-> danc, loving-> love
+#     y.clear()
+
+#     for i in text:
+#         y.append(ps.stem(i))
+
+#     return " ".join(y)
+
+# tfidf = pickle.load(open('vectorizer.pkl','rb'))
+# model = pickle.load(open('model.pkl','rb'))
+
+# st.title("Email Spam Filter")
+
+# input_email= st.text_area("Enter the message")
+
+# if st.button('Predict'):
+#     # 1. preprocess
+#   transformed_email = transform_text(input_email)
+#   # 2. vectorize
+#   vector_input = tfidf.transform([transformed_email])
+#   # 3. predict
+#   result = model.predict(vector_input)[0]
+#   # 4. display
+#   if result==1:
+#       st.header("Spam")
+#   else:
+#       st.header("Not Spam")
 import streamlit as st
 import pickle
 import string
@@ -5,27 +59,33 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+# Download required NLTK data
+@st.cache_resource
+def load_nltk():
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+load_nltk()
+
 ps = PorterStemmer()
 
 def transform_text(text):
     text = text.lower()
-    # breaking into separate words
     text = nltk.word_tokenize(text)
 
-    # as text is converted to list after tokenization- so using loop
     y = []
     for i in text:
-        if i.isalnum():  # just include alphanumeric- remove special characters
+        if i.isalnum():
             y.append(i)
 
-    text = y[:]  # removing stopwords and punctuation
+    text = y[:]
     y.clear()
 
     for i in text:
         if i not in stopwords.words("english") and i not in string.punctuation:
             y.append(i)
 
-    text = y[:]  # stemming  dancing-> danc, loving-> love
+    text = y[:]
     y.clear()
 
     for i in text:
@@ -38,18 +98,14 @@ model = pickle.load(open('model.pkl','rb'))
 
 st.title("Email Spam Filter")
 
-input_email= st.text_area("Enter the message")
+input_email = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    # 1. preprocess
-  transformed_email = transform_text(input_email)
-  # 2. vectorize
-  vector_input = tfidf.transform([transformed_email])
-  # 3. predict
-  result = model.predict(vector_input)[0]
-  # 4. display
-  if result==1:
-      st.header("Spam")
-  else:
-      st.header("Not Spam")
+    transformed_email = transform_text(input_email)
+    vector_input = tfidf.transform([transformed_email])
+    result = model.predict(vector_input)[0]
 
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
